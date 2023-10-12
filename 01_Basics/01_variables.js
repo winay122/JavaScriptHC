fae7441e50852a179af7fdaef26ade67524363f1
Vinay Kumar <108994206+winay122@users.noreply.github.com>
2023-10-12 05:09:57 +0000

    variables and datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "vinay@gmail.com"
+var accountPassword = "54321"
+let accountState;
+accountCity = "Jaipur"
+
+// accountId = 56789  // not allowed
+accountEmail = "win@gmail.com"
+accountPassword = "12345"
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all  JS as newer version.
+
+// alert("somthing went wrong")  //we are using nodejs, not a browser
+
+console.log(3+3)
+console.log('Vinay')
+
+let name = "Vinay"
+let age = 18
+let isLoggedIn = false
+
+// DATA TYPES
+
+//number 
+//bigint
+//string
+//boolean //tru or false
+//null  // standalone value
+//undefined 
+//symbol - unique
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  // object
